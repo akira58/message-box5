c2be1c4d973bf2c5aef827edfdc27773a8632fba
akira <akira.github@gmail.com>
2018-07-27 10:51:58 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+// Route::get('messages/{id}','MessagesController@show');
+// Route::post('messages','MessagesController@store');
+// Route::put('messages/{id}','MessagesController@update');
+// Route::delete('messages/{id}','MessagesController@destroy');
+// Route::get('messages','MessagesController@index');
+// Route::get('message/create', 'MessagesController@create');
+// Route::get('message/{id}/edit', 'MessagesController@edit')->name('messages.edit');
+
+Route::resource('messages', 'MessagesController');
